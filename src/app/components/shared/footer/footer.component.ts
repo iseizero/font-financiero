8b584597503bd892c4511b5658c843613dd4c776
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-15 22:43:23 -0400

    Agregando nav y footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnChanges } from '@angular/core';
+import * as moment from 'moment';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  currentDate: any;
+  currentHour: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.currentDate = moment().format('L');
+    setInterval(() => {
+      this.currentHour = moment().format('LTS');
+    }, 1000);
   }
 
 }
