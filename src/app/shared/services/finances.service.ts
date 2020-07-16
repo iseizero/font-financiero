b41c8db26f323c3bc2e5806098eb63e44d3be6d8
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-15 23:12:22 -0400

    componente card

---
+++ src/app/shared/services/finances.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinancesService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllMetrics(): Observable<any> {
+    return this.http.get(`http://localhost:4020/init`);
+  }
+}
