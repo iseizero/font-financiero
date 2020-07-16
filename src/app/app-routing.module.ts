f90ef6efed4e13485cde66ac8ae567913a69b05e
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-15 22:10:38 -0400

    Components init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { InitComponent } from './components/init/init.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'init', component: InitComponent },
+  { path: '**', component: InitComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
